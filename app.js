e869dfa4c3339953a95c38263f988ebfcf1d3ee6
Adrienne Johnson <adriennetjohnson@gmail.com>
2018-09-21 15:42:23 -0400

    synced with database

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const views = require('./views');
-
+const { Page, User } = require('./models');
 
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
@@ -13,8 +13,20 @@ app.get('/', (req, res, next) => {
   res.send(views.main(''));
 });
 
+// db.authenticate().
+// then(() => {
+//   console.log('connected to the database');
+// })
+
 const PORT = 3000;
 
-app.listen(PORT, () => { 
-  console.log(`Listening on port ${PORT}`);
-})
+const init = async () => {
+  await Page.sync();
+  await User.sync();
+
+  app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+  })
+}
+
+init();

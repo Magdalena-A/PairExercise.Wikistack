f8cfbfce21aa24893eb4f826db6eca0aa8d5adf5
Magdalena Aichinger <magdalena.aichinger@gmail.com>
2018-09-21 12:09:50 -0400

    setup complete

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const views = require('./views');
 
 
 app.use(morgan('dev'));
@@ -9,10 +10,10 @@ app.use(express.json());
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res, next) => {
-  res.send('Hello World');
+  res.send(views.main(''));
 });
 
-const PORT = 1444;
+const PORT = 3000;
 
 app.listen(PORT, () => { 
   console.log(`Listening on port ${PORT}`);
